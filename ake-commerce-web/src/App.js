8776f570e3e98726878adb5d3b95de68ae6589e6
an1262000 <KhangHiHi12062000>
2022-03-03 17:12:53 +0700

    Design Login Screen

--- ake-commerce-web/src/App.js
+++ ake-commerce-web/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className='Login-Container'>
+      <nav className='NavBar-Login'> NavBar-Login </nav>
+      <div className='Board-Login'> Board-Login </div>
+      <footer className='NavBar-Footer'> NavBar-Footer </footer>
     </div>
   );
 }
